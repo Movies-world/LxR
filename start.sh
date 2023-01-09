1e005ede8977911136e86b03bc0c36a3cdc8e45f
༒ᶜʳᵃᶻʸᴮᴼˢˢ卂乃卄丨丂卄乇Ҝ༒ <93928612+200920082007@users.noreply.github.com>
2023-01-09 20:50:55 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/EvamariaTG/EvaMaria.git /EvaMaria
+  git clone https://github.com/200920082007/LXR_EvaMaria.git /app
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /EvaMaria
+  git clone $UPSTREAM_REPO /app
 fi
-cd /EvaMaria
+cd /app
 pip3 install -U -r requirements.txt
-echo "Starting Bot...."
-python3 bot.py
+echo "Starting LXR BY LXR_YOUTUBER 🔥🎉...."
+python3 LXR.py
