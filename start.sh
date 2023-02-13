d4cd5f896712484496bfa77b1938e14953659000
Movies-world <121082436+Movies-world@users.noreply.github.com>
2023-02-13 17:21:48 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/Movie-world/MW-rename.git /app
+  git clone https://github.com/Movie-world/MW-rename.git /MW-RENAME
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /app
+  git clone $UPSTREAM_REPO /MW-RENAME
 fi
-cd /app
+cd /MW-RENAME
 pip3 install -U -r requirements.txt
 echo "Starting LXR BY LXR_YOUTUBER 🔥🎉...."
 python3 LXR.py
